669e1610d639b7fb1bc37bd16688aa1e7e96f651
Amos Baynes <Amos.Baynes@acumenmd.com>
2018-06-08 10:01:46 -0400

    Add project files.

---
+++ WikiTests.Specs/RunSearches.feature
@@ -0,0 +1,21 @@
+﻿Feature: RunSearches
+	I want to run some searches on Wikipedia and confirm I get the correct results
+
+@firsttest
+Scenario: Find info on honey badger
+	Given I have navigated to the Wikipedia home page
+	When I search "honey badger"
+	Then the correct information page loads
+
+@firstoutline
+Scenario Outline: Find info on various search terms
+	Given I have navigated to the Wikipedia home page
+	When I search <searchTerm>
+	Then the <searchResult> information page loads
+
+	Examples: 
+	| searchTerm  | searchResult     |
+	| taco        | Taco             |
+	| Löwen       | Löwen            |
+	| नमस्ते        | Namaste          |
+	| fighter jet | Fighter Aircraft |
---
+++ WikiTests.Specs/RunSearchesSteps.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using TechTalk.SpecFlow;
+
+namespace WikiTests.Specs
+{
+    [Binding]
+    public class RunSearchesSteps
+    {
+        [Given(@"I have navigated to the Wikipedia home page")]
+        public void GivenIHaveNavigatedToTheWikipediaHomePage()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [When(@"I search ""(.*)""")]
+        public void WhenISearch(string searchTerm)
+        {
+            ScenarioContext.Current.Pending();
+        }
+
+        [When(@"I search ""(.*)""")]
+        public void WhenISearchTaco()
+        {
+            ScenarioContext.Current.Pending();
+        }
+
+        [Then(@"the correct information page loads")]
+        public void ThenTheCorrectInformationPageLoads()
+        {
+            ScenarioContext.Current.Pending();
+        } 
+
+        [Then(@"the ""(.*)"" information page loads")]
+        public void ThenTheTacoInformationPageLoads()
+        {
+            ScenarioContext.Current.Pending();
+        }
+
+    }
+}
